488a3813889e72c99509e75f2aac13e83973d1ab
xiaowenqiang <1492634467@qq.com>
2025-03-18 10:19:09 +0800

    修改配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://xwqsean.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'xwqsean', // Usually your GitHub org/user name.
+  projectName: 'xwqsean.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'dev',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
